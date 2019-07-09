63ca4082f20304e3bbb48c20784355165525537f
Noble Obioma <nobioma1@gmail.com>
2019-07-09 13:50:26 +0100

    Create Server index file and routes file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const routes = require('./routes');
+const postsRoutes = require('./postsRoutes');
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.use(routes.posts, postsRoutes);
+
+server.get(routes.base, (req, res) => {
+  res.status(200).send('Welcome to Posts');
+});
+
+server.listen(PORT, () => {
+  console.log(`server live @ http://localhost:${PORT}`);
+});
---
+++ postsRoutes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const postRoutes = express.Router();
+
+postRoutes.post('/', (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+
+// postRoutes.post()
+
+// postRoutes.get()
+
+// postRoutes.get()
+
+// postRoutes.get()
+
+// postRoutes.get()
+
+// postRoutes.put()
+
+
+module.exports = postRoutes;
