21ab2df2b77cb4bc898a7e47bc88bdd5eda7385c
Noble Obioma <nobioma1@gmail.com>
2019-07-09 14:08:10 +0100

    Create add post route

--- postsRoutes/index.js
+++ postsRoutes/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 
+const Posts = require('../data/db');
+
 const postRoutes = express.Router();
 
-postRoutes.post('/', (req, res) => {
+postRoutes.post('/', async (req, res) => {
+  const { title, contents } = req.body;
+  
   try {
-    
+    const newPost = { title: title.trim(), contents: contents.trim() };
+
+    if (newPost.title && newPost.contents) {
+      const postId = await Posts.insert(newPost);
+      const post = await Posts.findById(postId.id);
+      res.status(201).json({ ...post[0] });
+    } else {
+      res.status(400).json({
+        errorMessage: 'Please provide title and contents for the post.',
+      });
+    }
   } catch (error) {
-    
+    res.status(500).json({
+      error: 'There was an error while saving the post to the database',
+    });
   }
-})
+});
 
 // postRoutes.post()
 
@@ -21,6 +37,5 @@ postRoutes.post('/', (req, res) => {
 // postRoutes.get()
 
 // postRoutes.put()
-
 
 module.exports = postRoutes;
